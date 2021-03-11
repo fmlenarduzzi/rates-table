a93c6ffb3abf3d89cc5da5a4160f787f2dd5ea56
Federico Lenarduzzi <flenarduzzi@simtlix.com>
2021-03-10 23:19:45 -0300

    Calculate Fx methods were added.

--- src/app.js
+++ src/app.js
@@ -5,8 +5,37 @@ const Rate = require('./models/Rate')
 
 const API_KEY = "ffcc344a3f31700c0020d166fd17ea96";
 
-function populateDB() {
-    http.get('http://data.fixer.io/api/latest?access_key=' + API_KEY, (resp) => {
+async function calculateRate(from, to, fee) {
+    const ratesList = await getFxRates();
+
+    let originalRate;
+    let feeAmount;
+    let rateWithFee;
+
+    if (from === 'EUR') {
+        originalRate = ratesList[to];
+        feeAmount = originalRate * (1 / fee);
+        rateWithFee = originalRate + feeAmount;
+    } else {
+
+    }
+    /*Object.keys(ratesList).forEach(async function(key) {
+        const query = {rate: key},
+            update = {price: ratesList[key]},
+            options = { upsert: true, new: true, setDefaultsOnInsert: true };
+        await Rate.findOneAndUpdate(query, update, options);
+    }); */
+    return {
+        "Pair" : from+to,
+        "Original Rate" : originalRate,
+        "Fee %" : fee,
+        "Fee Amount" : feeAmount,
+        "Rate with fee applied" : rateWithFee
+    }
+}
+
+async function getFxRates() {
+    await http.get('http://data.fixer.io/api/latest?access_key=' + API_KEY, (resp) => {
         let data = '';
 
         // A chunk of data has been received.
@@ -16,13 +45,7 @@ function populateDB() {
 
         // The whole response has been received. Print out the result.
         resp.on('end',  () => {
-            const ratesList = JSON.parse(data).rates;
-            Object.keys(ratesList).forEach(async function(key) {
-                const query = {rate: key},
-                    update = {price: ratesList[key]},
-                    options = { upsert: true, new: true, setDefaultsOnInsert: true };
-                await Rate.findOneAndUpdate(query, update, options);
-            });
+            return JSON.parse(data).rates;
         });
 
     }).on("error", (err) => {
@@ -41,31 +64,52 @@ const init = async () => {
         method: 'GET',
         path: '/rates',
         handler: async (request, h) => {
+            let result = [];
             const rates = await Rate.find();
-            return h.response(rates);
+            for (const rate of Object.keys(rates)) {
+                console.log('rate = ' + rate );
+                const fxRate = await calculateRate(rate.from, rate.to, rate.feePercentage);
+                console.log('fxRate = ' + JSON.stringify(fxRate));
+                result.push(fxRate);
+            }
+            return h.response(result);
         }
     }) // end of server.route
 
     server.route({
         method: 'GET',
-        path: '/rates/{rate}',
+        path: '/rates/{from}/{to}',
         handler: async (request, h) => {
-            const foundRate = await Rate.find({rate: request.params.rate});
-            return h.response(foundRate);
+            const foundRates = await Rate.find({from: request.params.from, to: request.params.to});
+            return h.response(foundRates);
         }
     }) // end of server.route
 
     server.route({
         method: 'POST',
-        path: '/pair',
+        path: '/rates',
         handler: async (request, h) => {
-
+            try {
+                const rate = new Rate(request.payload)
+                const rateSaved = await rate.save();
+                return h.response(rateSaved);
+            } catch (error) {
+                console.log(error.message);
+                let errorMsg = '';
+                if (error.message.includes('is not a valid enum value')) {
+                    errorMsg = "Invalid Data";
+                } else if (error.message.includes('duplicate key error collection')){
+                    errorMsg = 'Rate already existed.';
+                } else {
+                    errorMsg = error;
+                }
+                return h.response(errorMsg).code(500);
+            }
         }
     }) // end of server.route
 
     await server.start();
     console.log('Server running on %s', server.info.uri);
-    populateDB();
 };
 
 init();
